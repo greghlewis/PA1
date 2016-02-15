ab18e08f25651bdd8ce8fea7b5ecaa88be7d3228
Greg Lewis <greghlewis@outlook.com>
2016-02-15 10:41:04 -0700

    First attempt at using version control

---
+++ greg/src/main/java/greg/ProgAssign1.java
@@ -0,0 +1,17 @@
+package greg;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ProgAssign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
